1a60f746a015ac5a9895b6577df32348dbb2abe2
zyuhung <zzh_myself@126.com>
2016-09-08 15:41:57 +0800

    add an idx of best at the "method 2"

--- kernel.cu
+++ kernel.cu
@@ -6,12 +6,18 @@
 
 __global__ void GetMaxNum_kernel(int *num, int *max)
 {
+	__shared__ int *best;
+	best = (int*)malloc(sizeof(int)* 20);
 	int tid = blockDim.x*blockIdx.x + threadIdx.x;
 	int tid_idx = 2;
 	//************ Method 2 (Better) ************
 	while ((tid_idx<= pow((float)2,(int)sqrt((float)20)+1)) &&(tid*tid_idx<20))
 	{
-		num[tid*tid_idx] = (num[tid*tid_idx] >= num[tid*tid_idx + tid_idx/2]) ? num[tid*tid_idx] : num[tid*tid_idx + tid_idx/2];
+		if (num[tid*tid_idx] < num[tid*tid_idx + tid_idx / 2])
+		{
+			num[tid*tid_idx] = num[tid*tid_idx + tid_idx / 2];
+			best[tid*tid_idx] = tid*tid_idx + tid_idx / 2;
+		}
 		tid_idx *= 2;
 		__syncthreads();
 	}
@@ -36,6 +42,7 @@ __global__ void GetMaxNum_kernel(int *num, int *max)
 	}*/
 
 	*max = num[0];
+	printf("best num idx is :%d\n", best[0]);
 }
 
 int main()
